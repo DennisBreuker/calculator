7c680cdf41ba294ac7803c589ec7d91f7fdf17b7
Dennis Breuker <dennis@dennisbreuker.nl>
2024-10-03 13:11:10 +0200

    Commented some code

--- src/main/java/nl/han/aim/asd/parser/ExpressionReader.java
+++ src/main/java/nl/han/aim/asd/parser/ExpressionReader.java
@@ -9,11 +9,11 @@ import java.util.Stack;
  * Created by Michel Koolwaaij on 10-10-18.
  */
 public class ExpressionReader extends ExpressionsBaseListener {
-    Stack<Expression> expressions = new Stack<Expression>();
+//    Stack<Expression> expressions = new Stack<Expression>();
 
-    public Expression getTopExpression() {
-        return expressions.peek();
-    }
+//    public Expression getTopExpression() {
+//        return expressions.peek();
+//    }
 
     // TODO: 4.3 1b -- Maak een Antlr listener klasse waarmee je
     //  de geparseerde sommen omzet naar de bijbehorende AST’s.
